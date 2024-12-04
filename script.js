bf2e04839df312923dad01c683bdb37631bd7e52
Darwinalgorithm <geraldfournier42@gmail.com>
2024-12-05 00:41:50 +0100

    fetch irrégulier....

--- script.js
+++ script.js
@@ -31,33 +31,127 @@ console.log("Services :", services);
 console.log("Témoignages :", temoignages);
 //je suis étonné le console log doit être dans l'accolade.... sinon mes données n'apparaissent plus 
 //pourquoi ??? je pensais que les console.log étaient indépendants et permettaient de vérifier les choses
+//c du genre la donnée n'existe que dans then pour l'instant, let et pas var....
     });
 
 //bref, ça marche on peut passer à l'affichage de ces données dans le dom. 
-//je cible à présent la section service pour effectuer le processus, j'englobe la section.
+//je cible à présent la section service, en particulier la div pour effectuer le processus.
+//on n'oublie pas les antiquotes !
 
-let CarteServiceAdupliquer =
-<section>
-<h2>SERVICES</h2>
+// face aux difficultés : fonction fondamentale :
 
-<div class="containerGrid spaceBetween" id="containerServices">
-    <div class="box">
-        <h3>texte</h3>
-        <p>Service 1 description</p>
-    </div>
-    <div class="box">
-        <h3>texte</h3>
-         <p>Service 2 description</p>
-    </div>
-    <div class="box"> 
-        <h3>texte</h3>
-        <p>Service 3 description</p>
-    </div>
-</div>
-</section>;
 
+    function ajouterElementsAuDOM(liste, conteneurId, genererHTML) {
+        const conteneur = document.querySelector(`#${conteneurId}`);
+      
+        // Vide le conteneur pour éviter les doublons
+        conteneur.innerHTML = '';
+      
+        // Ajoute chaque élément au conteneur
+        liste.forEach(item => {
+          conteneur.innerHTML += genererHTML(item);
+        });
+      }
+      
+      function genererHTMLPromesse(promesse) {
+        return `<p>${promesse}</p>`;
+      }
+      
+      fetch('./restaurant-bresilien.json')
+        .then(response => response.json())
+        .then(donnee => {
+          // Ajoute les promesses clients au conteneur
+          ajouterElementsAuDOM(donnee.promessesClient, 'containerPromesses', genererHTMLPromesse);
+        })
+        .catch(error => console.error("Erreur lors du chargement :", error));
+        
+        fetch('./restaurant-bresilien.json')
+        .then(response => response.json())
+        .then(donnee => {
+          // Services
+          ajouterElementsAuDOM(donnee.services, 'containerServices', genererHTMLService);
+      
+          // Promesses clients
+          ajouterElementsAuDOM(donnee.promessesClient, 'containerPromesses', genererHTMLPromesse);
+      
+          // Témoignages
+          ajouterElementsAuDOM(donnee.temoignages, 'containerTemoignages', genererHTMLTemoignage);
+        })
+      
+        function genererHTMLTemoignage(temoignage) {
+            return `
+              <div class="box">
+                <h4>${temoignage.prenom}</h4>
+                <p>${temoignage.typeExperience}</p>
+                <p>${temoignage.commentaire}</p>
+                <p>Note : ${temoignage.note}</p>
+              </div>
+            `;
+          }
+
+    /*
+fetch('./restaurant-bresilien.json')
+  .then(response => response.json())
+  .then(donnee => {
+
+    let ConteneurHTMLService = document.getElementById('containerServices');
+
+
+    donnee.services.forEach(service => {
+      const serviceHTML = `
+        <div class="box">
+          <h3>${service.nom}</h3>
+          <p>${service.desc}</p>
+        </div>
+      `;
+      ConteneurHTMLService.innerHTML += serviceHTML;
+    });
+  })
 
 document.querySelector("#containerServices").innerHTML+=CarteServiceAdupliquer;
-//c'est la formule que Ludivine m'a montré et que j'aime bien car condensée.
+//c'est la formule que Ludivine m'a montré et que j'aime bien car condensée
+
+//Passons aux témoignages
 
 
+    function genererHTMLPromesse(promesse) {
+        return `<p>${promesse}</p>`;
+      }
+      fetch('./restaurant-bresilien.json')
+  .then(response => response.json())
+  .then(donnee => {
+    // Ajoute les promesses clients au conteneur
+    ajouterElementsAuDOM(donnee.promessesClient, 'containerPromesses', genererHTMLPromesse);
+  })*/
+
+
+
+
+
+
+
+
+// je passe aux avis clients 
+
+
+/*
+fetch('./restaurant-bresilien.json')
+  .then(response => response.json())
+  .then(donnee => {
+
+    let conteneurHTMLTemoignages = document.getElementById('containerTemoignages');
+  
+    donnee.temoignages.forEach(temoignage => {
+      const CarteTemoignageAdupliquer = ` 
+    <div class="box">
+        <h4>${temoignage.prenom}</h4>
+        <p>${temoignage.typeExperience}</p>
+        <p>${temoignage.commentaire}</p>
+        <p>${temoignage.note}</p>
+    </div>
+    `;
+    conteneurHTMLTemoignages.innerHTML += CarteTemoignageAdupliquer;
+  });
+})
+
+document.querySelector("#containerTemoignages").innerHTML+=CarteTemoignageAdupliquer;*/
